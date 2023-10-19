cd2986229ab083d50217dd0aada96f851194808c
Nick8449 <ovchynnik.mykola@gmail.com>
2023-10-19 20:59:33 +0200

    gallery-1 upd
    modal window closed by Escape btn

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,13 @@
-// import * as basicLightbox from "basiclightbox";
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
 
 gallery.addEventListener("click", handlerClick);
 
 createMarkup(galleryItems, gallery);
+
+let modal;
 
 function createMarkup(galleryItems, targetElement) {
   const markup = galleryItems
@@ -39,12 +38,26 @@ function handlerClick(event) {
 
   const originalImage = event.target.dataset.source;
 
-  crateModal(originalImage);
+  modal = crateModal(originalImage);
+  modal.show();
+
+  if (modal.visible()) {
+    document.addEventListener("keydown", handlerEscape);
+  }
 }
 
 function crateModal(originalImage) {
-  const instance = basicLightbox.create(`
-      <img src="${originalImage}" width="800" height="600">`);
+  const newInstance = basicLightbox.create(
+    `<img src="${originalImage}" width="800" height="600">`
+  );
 
-  instance.show();
+  return newInstance;
+}
+
+function handlerEscape(event) {
+  if (event.code === "Escape") {
+    modal.close();
+
+    document.removeEventListener("keydown", handlerEscape);
+  }
 }
