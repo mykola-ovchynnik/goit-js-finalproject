bcfd5c447db2a9d8eec8bea9b90965e01d0fa6c0
Nick8449 <ovchynnik.mykola@gmail.com>
2023-10-19 20:02:41 +0200

    gallery-1
    crated markup
    click handler for image
    modal markup

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+// import * as basicLightbox from "basiclightbox";
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+gallery.addEventListener("click", handlerClick);
+
+createMarkup(galleryItems, gallery);
+
+function createMarkup(galleryItems, targetElement) {
+  const markup = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>`
+    )
+    .join("");
+
+  targetElement.insertAdjacentHTML("afterbegin", markup);
+}
+
+function handlerClick(event) {
+  event.preventDefault();
+
+  if (event.currentTarget === event.target) {
+    return;
+  }
+
+  const originalImage = event.target.dataset.source;
+
+  crateModal(originalImage);
+}
+
+function crateModal(originalImage) {
+  const instance = basicLightbox.create(`
+      <img src="${originalImage}" width="800" height="600">`);
+
+  instance.show();
+}
