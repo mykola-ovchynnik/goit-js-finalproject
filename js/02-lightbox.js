6fa68acbc8bed3e0d437e03e7b2c64916cc19b4c
Nick8449 <ovchynnik.mykola@gmail.com>
2023-10-21 16:34:21 +0200

    simple-light-box

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+createMarkup(galleryItems, gallery);
+
+function createMarkup(galleryItems, targetElement) {
+  const markup = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+        />
+        </a>
+        </li>`
+    )
+    .join("");
+
+  targetElement.insertAdjacentHTML("afterbegin", markup);
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
